e03598d06a9c50aef94e5b5ac4eafaf95ee0925b
shao0213 <B10823055@gemail.yuntech.edu.tw>
2022-07-21 04:33:30 +0800

    成員介紹 card + 一些修正

--- Frontend/urls.py
+++ Frontend/urls.py
@@ -19,4 +19,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('members/', views.members, name="members"),
+    path('appointment/', views.appointment, name="appointment"),
 ]
--- Frontend/views.py
+++ Frontend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 # 首頁
 def index(request):
     # news = News.objects.all().order_by('id')
@@ -8,3 +9,19 @@ def index(request):
     #     'news':news
     # }
     return render(request, "index/index.html")
+
+# 成員
+def members(request):
+    # news = News.objects.all().order_by('id')
+    # context = {
+    #     'news':news
+    # }
+    return render(request, "members/members.html")
+
+# 預約友誼賽
+def appointment(request):
+    # news = News.objects.all().order_by('id')
+    # context = {
+    #     'news':news
+    # }
+    return render(request, "appointment/appointment.html")
