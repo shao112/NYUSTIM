e7b84ef3acd5a1ee6077d423996f515e7b4a358e
shao0213 <B10823055@gemail.yuntech.edu.tw>
2022-07-29 16:25:04 +0800

    header的RWD

--- Frontend/views.py
+++ Frontend/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Members, Competition, Appointment, Events
+# 分頁
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -23,6 +25,16 @@ def members(request):
 def competition(request):
 
     competition = Competition.objects.all().order_by('id')
+    # 顯示 6 筆資料
+    paginator = Paginator(competition, 6)
+    # 獲取 url 中的頁碼，比如第一頁，我們需要在url末尾中新增 ?page=1
+    page = request.GET.get('page')
+    # 獲取相應的頁碼的資料，比如page=1，第一頁，這裡獲取得到第一頁的資料內容
+    competition = paginator.get_page(page)
+    # 獲取一共分出來了多少頁，前端展示所有頁碼數的時候需要用到該數
+    print("總共有",paginator.num_pages,"頁")
+    print("多少資料",paginator.object_list)
+    print(competition.paginator.page_range)
 
     context = {
         'competition':competition
