28fa5da45ca44a55dfd84247050a88935d62be3f
shao0213 <B10823055@gemail.yuntech.edu.tw>
2022-08-02 18:16:36 +0800

    更新圖片+header的RWD細修

--- Frontend/views.py
+++ Frontend/views.py
@@ -58,6 +58,17 @@ def events(request):
     
     events = Events.objects.all().order_by('id')
     
+    # 顯示 6 筆資料
+    paginator = Paginator(events, 10)
+    # 獲取 url 中的頁碼，比如第一頁，我們需要在url末尾中新增 ?page=1
+    page = request.GET.get('page')
+    # 獲取相應的頁碼的資料，比如page=1，第一頁，這裡獲取得到第一頁的資料內容
+    events = paginator.get_page(page)
+    # 獲取一共分出來了多少頁，前端展示所有頁碼數的時候需要用到該數
+    print("總共有",paginator.num_pages,"頁")
+    print("多少資料",paginator.object_list)
+    print(events.paginator.page_range)
+
     context = {
         'events':events
     }
